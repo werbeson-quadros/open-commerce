c95fd630682632a0a9a381dee11bdf0dc5713d82
bsongit <besonquadros@gmail.com>
2026-07-05 16:10:23 -0300

    fet(source): add project structure from source

---
+++ source/src/main/java/com/werbeson/opencommerce/OpencommerceApplication.java
@@ -0,0 +1,13 @@
+package com.werbeson.opencommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpencommerceApplication.class, args);
+	}
+
+}
---
+++ source/src/test/java/com/werbeson/opencommerce/OpencommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.werbeson.opencommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
